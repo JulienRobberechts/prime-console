eb18db373d2520ae09cc7ad7585c06c530821b9a
angledor <angledor-git@yahoo.fr>
2018-12-20 20:01:09 +0000

    repl edge cases

--- views/consoleRepl.js
+++ views/consoleRepl.js
@@ -4,13 +4,31 @@ const primes = require('../lib/primes');
 exports.launch = () => {
   const replServer = repl.start({
     prompt: "prime-console > ",
+    writer: o => o, // useful to convert the output
   });
 
   // i.e: primes(100)
   Object.defineProperty(replServer.context, 'primes', {
     configurable: false,
     enumerable: true,
-    value: (limit) => primes.findPrimes(limit),
+    value: (limit) => tryCmd(() => primes.findPrimes(checkInt(limit))),
   });
 
+}
+
+const tryCmd = (cmd) => {
+  try {
+    return cmd();
+  } catch (error) {
+    return "invalid command - " + error;
+  }
+}
+
+const checkInt = (stringArg) => {
+  const value = eval(stringArg);
+  var integer = parseInt(value, 10);
+  if (isNaN(integer)) {
+    throw `'${JSON.stringify(value)}' is not a valid integer parameter.`;
+  }
+  return integer;
 }
