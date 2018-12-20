2c3d9660a980b60fa456953a0b4dcb2e39787057
angledor <angledor-git@yahoo.fr>
2018-12-20 21:41:41 +0000

    table format - imp

--- views/tableView.js
+++ views/tableView.js
@@ -1,5 +1,17 @@
 const format = (table) => {
-  return [];
+  if (!table) {
+    return '';
+  }
+
+  var tableAsString = table.reduce((output, line) => {
+    return `${output}| ${formatLine(line)}|\n`;
+  }, "");
+
+  return tableAsString;
+};
+
+const formatLine = line => {
+  return line.join('| ');
 };
 
 module.exports = {
